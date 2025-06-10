ad597cbcf23630b480e38b652d8c145ca66f24a4
Alex Menezes <alex.menezes.one@gmail.com>
2025-06-09 22:13:21 -0300

    Adicione arquivo .gitignore

---
+++ modulo_1/Estrutura_de_dados_Listas.py
@@ -0,0 +1,11 @@
+# Listas em Python
+# Definindo uma lista de frutas
+frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"]
+
+# Acessando elementos da lista
+print(frutas[0])  # Imprime na tela o primeiro elemento -> maçã
+print(frutas[1])  # Imprime na tela o segundo elemento da lista -> banana
+print(frutas[2])  # Imprime na tela o terceiro elemento da lista -> laranja
+print(frutas[3])  # Imprime na tela o quarto elemento da lista -> uva
+print(frutas[4])  # Imprime na tela o quinto elemento da lista -> abacaxi
+
